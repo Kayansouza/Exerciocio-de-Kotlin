67ebc2e19f91e298acc18d86c43478a2eca92ba8
Kayansouza <rick.kayan@gmail.com>
2026-03-02 15:06:30 -0300

    Primeiro commit - Exercício Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+
+    var nome_do_carro = "Fusca"
+
+    println(nome_do_carro)
+  val nome = readLine()
+
+    println("O nome do carro é: $nome")
+}
